f5546fd47a86ab12c5df46dfbea031b93b5f81c8
NBudyaev <nikita.budyaev.f@gmail.com>
2023-09-27 15:51:21 +0400

    First comm

---
+++ src/main/java/com/learning/testprojectlearning/TestProjectLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.testprojectlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/testprojectlearning/TestProjectLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.testprojectlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
